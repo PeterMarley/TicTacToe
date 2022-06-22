0d4675f6ea570d354bee4c0ebed6afdfd5abf0bc
BigJeffTheChef <marleygpeter@gmail.com>
2022-06-22 17:21:32 +0100

    refactored, to bring it closer to specs, using more IIFE modules and factories

--- game.js
+++ game.js
@@ -1,155 +1,162 @@
-// global declarations
-const BOARD_SIZE = 3;
-const PLAYERS = 2;
-
-let players = [];
-
-let gameOver, boardArr, introElement, boardElement;
-const contentDiv = document.querySelector('.content');
+// game controls module implemented with IIFE
+const gameControls = (() => {
+    const BOARD_SIZE = 3;
+    let players = [];
+    let gameOver, boardArr, introElement, currentPlayer;
+    const contentElement = document.querySelector('.content');
+    return {
+        BOARD_SIZE,
+        players,
+        gameOver,
+        boardArr,
+        introElement,
+        currentPlayer,
+        contentElement
+    };
+})();
 
 // start game for first time
 start();
 
 /*
 
-    INTRODUCTION
+    START GAME
 
 */
 
+/**
+ * Starts a new game. First this function removes the old board and intro divs (if they exist), and appends new ones to the contentElement
+ */
 function start() {
-    gameOver = false;
 
-    const boardOld  = document.querySelector('div.container-board');
-    if (boardOld !== null) contentDiv.removeChild(boardOld); 
+    // ensure new gameOver flag is set to false
+    gameControls.gameOver = false;
+
+    // remove old board and intro
+    const boardOld = document.querySelector('div.container-board');
+    if (boardOld !== null) gameControls.contentElement.removeChild(boardOld);
 
     const introOld = document.querySelector('div.container-intro');
-    if (introOld !== null) contentDiv.removeChild(introOld);
+    if (introOld !== null) gameControls.contentElement.removeChild(introOld);
 
-    boardArr = createBoard(BOARD_SIZE);
-
-    introElement = createIntro();
-    contentDiv.appendChild(introElement);
-}
-
-function createIntro() {
-
-    const introp1 = createP('Hello, welcome to Tic-Tac-Toe. Get 3 in a line to win (horizontally, vertically or diagonally).');
-    const introp2 = createP('Please enter the player names below');
-    const form = (() => {
-        const form = document.createElement('form');
-        form.classList.add('name-select-form');
-        form.appendChild(createInput(1));
-        form.appendChild(createInput(2));
-        return form;
-
-        function createInput(playerNumber) {
-            const label = document.createElement('label');
-            label.setAttribute("for", `player${playerNumber}-name`);
-            label.textContent = `Enter Player ${playerNumber} Name:`;
-            const input = document.createElement('input');
-            input.setAttribute('type', 'text');
-            input.setAttribute('id', `player${playerNumber}-name`);
-            input.setAttribute('required', '');
-            input.setAttribute('pattern', '^[A-Za-z]+$');
-            label.appendChild(input);
-            return label;
+    // create new board array, and append new intro to contentElement
+    gameControls.boardArr = ((size) => {
+        const boardObj = [];
+        for (let i = 0; i < size; i++) {
+            const row = [];
+            for (let j = 0; j < size; j++) {
+                row.push(null);
+            }
+            boardObj.push(row);
         }
+        return boardObj;
+    })(gameControls.BOARD_SIZE);
+
+    // create new intro div
+    gameControls.introElement = (() => {
+        // create intro text p elements
+        const introp1 = createP('Hello, welcome to Tic-Tac-Toe. Get 3 in a line to win (horizontally, vertically or diagonally).');
+        const introp2 = createP('Please enter the player names below');
+
+        function createP(text) {
+            const p = document.createElement('p');
+            p.textContent = text;
+            return p;
+        }
+
+        // create player name form
+        const form = (() => {
+            const form = document.createElement('form');
+            form.classList.add('name-select-form');
+            form.appendChild(createInput(1));
+            form.appendChild(createInput(2));
+            return form;
+
+            function createInput(playerNumber) {
+                const label = document.createElement('label');
+                label.setAttribute("for", `player${playerNumber}-name`);
+                label.textContent = `Enter Player ${playerNumber} Name:`;
+                const input = document.createElement('input');
+                input.setAttribute('type', 'text');
+                input.setAttribute('id', `player${playerNumber}-name`);
+                input.setAttribute('required', '');
+                input.setAttribute('pattern', '^[A-Za-z]+$');
+                label.appendChild(input);
+                return label;
+            }
+        })();
+
+        // create submit button - which is actually outside the form
+        const buttonPlayerNames = (() => {
+            const btn = document.createElement('button');
+            btn.classList.add('player-name-accept');
+            btn.textContent = 'Play!';
+            btn.addEventListener('click', () => {
+                // process player names and show board
+                const p1Name = document.querySelector('#player1-name').value;
+                const p2Name = document.querySelector('#player2-name').value;
+                let p1Accept = validateName(p1Name);
+                let p2Accept = validateName(p2Name);
+                if (p1Accept && p2Accept) {
+                    const container = document.querySelector('.container-intro');
+                    container.classList.add('hidden');
+                    setTimeout(() => {
+                        gameControls.contentElement.removeChild(container);
+                        // prepareGame(p1Name, p2Name);
+                        ((p1Name, p2Name) => {
+                            gameControls.players = [new Player(1, p1Name, "X"), new Player(2, p2Name, "O")];
+                            gameControls.currentPlayer = gameControls.players[0];
+                            const containerBoard = document.createElement('div');
+                            const insideContainer = document.createElement('div');
+                            containerBoard.classList.add("container-board");
+                            insideContainer.id = "board";
+                            containerBoard.appendChild(insideContainer);
+
+                            const buttonRestart = document.createElement('button');
+                            buttonRestart.textContent = "Restart Game";
+                            buttonRestart.classList.add('player-name-accept');
+                            buttonRestart.id = 'restart-button';
+                            buttonRestart.addEventListener('click', start);
+
+                            containerBoard.appendChild(buttonRestart);
+                            gameControls.contentElement.appendChild(containerBoard);
+                            displayBoard(gameControls.boardArr);
+                        })(p1Name, p2Name);
+                    }, 250);
+                }
+
+                function validateName(nameToValidate) {
+                    if (nameToValidate && typeof nameToValidate !== 'string') {
+                        return false;
+                    }
+                    const REGEX = /^[A-Za-z]+$/;
+                    if (nameToValidate && nameToValidate.match(REGEX)) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            return btn;
+
+        })();
+
+        // create the html elements for the intro, place together and return
+        const intro = document.createElement('div');
+        intro.classList.add('intro');
+        intro.appendChild(introp1);
+        intro.appendChild(introp2);
+        intro.appendChild(form);
+        intro.appendChild(buttonPlayerNames);
+
+        const containerIntro = document.createElement('div');
+        containerIntro.classList.add('container-intro')
+        containerIntro.appendChild(intro);
+
+        return containerIntro;
     })();
-    const buttonPlayerNames = (() => {
-        const btn = document.createElement('button');
-        btn.classList.add('player-name-accept');
-        btn.textContent = 'Play!';
-        btn.addEventListener('click', processIntro);
-        return btn;
-    })();
-    // const buttonRestart = document.createElement('button');
-    // buttonRestart.classList.add('player-name-accept');
-    // buttonRestart.addEventListener('click', () => {
-    //     clearArray(winningCoords);
-    //     clearArray(players);
-    //     clearArray(board);
-    //     board = createBoard();
-    //     const body = document.querySelector('body');
-    //     body.removeChild(intro);
-        
-    // });
-    // containerIntro.appendChild(buttonRestart);
 
-
-    const intro = document.createElement('div');
-    intro.classList.add('intro');
-    intro.appendChild(introp1);
-    intro.appendChild(introp2);
-    intro.appendChild(form);
-    intro.appendChild(buttonPlayerNames);
-
-    const containerIntro = document.createElement('div');
-    containerIntro.classList.add('container-intro')
-    containerIntro.appendChild(intro);
-
-    return containerIntro;
-
-    function createP(text) {
-        const p = document.createElement('p');
-        p.textContent = text;
-        return p;
-    }
+    gameControls.contentElement.appendChild(gameControls.introElement);
 }
-
-function processIntro(event) {
-    const p1Name = document.querySelector('#player1-name').value;
-    const p2Name = document.querySelector('#player2-name').value;
-    let p1Accept = validateName(p1Name);
-    let p2Accept = validateName(p2Name);
-    if (p1Accept && p2Accept) {
-        const container = document.querySelector('.container-intro');
-        container.classList.add('hidden');
-        setTimeout(() => {
-            contentDiv.removeChild(container);
-            prepareGame(p1Name, p2Name);
-        }, 250);
-
-    }
-}
-
-function validateName(nameToValidate) {
-    if (nameToValidate && typeof nameToValidate !== 'string') {
-        return false;
-    }
-    const REGEX = /^[A-Za-z]+$/;
-    if (nameToValidate && nameToValidate.match(REGEX)) {
-        return true;
-    }
-    return false;
-}
-
-function prepareGame(p1Name, p2Name) {
-    players = [new Player(1, p1Name, "X"), new Player(2, p2Name, "O")];
-    currentPlayer = players[0];
-    const containerBoard = document.createElement('div');
-    const insideContainer = document.createElement('div');
-    containerBoard.classList.add("container-board");
-    insideContainer.id = "board";
-    containerBoard.appendChild(insideContainer);
-
-    const buttonRestart = document.createElement('button');
-    buttonRestart.textContent = "Restart Game";
-    buttonRestart.classList.add('player-name-accept');
-    buttonRestart.id = 'restart-button';
-    buttonRestart.addEventListener('click', start);
-
-    containerBoard.appendChild(buttonRestart);
-    contentDiv.appendChild(containerBoard);
-    displayBoard(boardArr);
-}
-
-/*
-
-    RESTART
-
-*/
-
-
 
 /*
 
@@ -185,30 +192,6 @@ const playerMethods = {
 }
 
 /**
- * Create a 2-d array populated with null values.
- * @param {Number} size the length of each inner array and outer array.
- * @returns a 2d array with each outer array length size, and each inner array length size, populated with null values.
- */
-function createBoard(size) {
-    const boardObj = [];
-    for (let i = 0; i < size; i++) {
-        const row = [];
-        for (let j = 0; j < size; j++) {
-            row.push(null);
-        }
-        boardObj.push(row);
-    }
-    return boardObj;
-}
-
-/**
- * Switch the active player of the game
- */
-function switchPlayer() {
-    currentPlayer = (currentPlayer.getNumber() === players[0].getNumber()) ? players[1] : players[0];
-}
-
-/**
  * Builds the board div element and adds appropriate event handlers
  * @param {2d array} gameBoard a 2d array of players pieces 
  */
@@ -230,17 +213,20 @@ function displayBoard(gameBoard) {
             cellDiv.dataset.indexCol = j;
             if (cellContents == null) {
                 cellDiv.classList.add("empty");
-                cellDiv.addEventListener('click', () => {
-                    if (!gameOver) {
-                        if (gameBoard[i][j] === null) {
-                            gameBoard[i][j] = currentPlayer;
+                cellDiv.addEventListener('click', () => clickSquare(i,j));
+                function clickSquare(row, col) {
+                    // click square
+                    if (!gameControls.gameOver) {
+                        if (gameBoard[row][col] === null) {
+                            gameBoard[row][col] = gameControls.currentPlayer;
                             cellDiv.classList.remove("empty");
-                            cellDiv.textContent = currentPlayer.getSymbol();
-                            checkWinner(gameBoard, boardDiv, i, j);
-                            switchPlayer();
+                            cellDiv.textContent = gameControls.currentPlayer.getSymbol();
+                            checkWinner(gameBoard, boardDiv, row, col);
+                            // switch player
+                            gameControls.currentPlayer = (gameControls.currentPlayer.getNumber() === gameControls.players[0].getNumber()) ? gameControls.players[1] : gameControls.players[0];
                         }
                     }
-                });
+                }
             } else {
                 cellDiv.classList.remove("empty");
                 cellDiv.textContent = cellContents.getSymbol();
@@ -270,7 +256,7 @@ function checkWinner(gameBoard, boardDiv, row, col) {
     processResult(winFound);
 
     function discernWinningNode(row, col) {
-        return (BOARD_SIZE * row) + (col + 1) - 1;
+        return (gameControls.BOARD_SIZE * row) + (col + 1) - 1;
     }
 
     function checkLinear(direction, coords) {
@@ -279,14 +265,14 @@ function checkWinner(gameBoard, boardDiv, row, col) {
             return;
         }
         let check = 0;
-        for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let i = 0; i < gameControls.BOARD_SIZE; i++) {
             let position = (direction === 'row') ? gameBoard[row][i] : gameBoard[i][col];
-            if (position !== null && position.getNumber() === currentPlayer.getNumber()) {
+            if (position !== null && position.getNumber() === gameControls.currentPlayer.getNumber()) {
                 check++;
                 coords.push(discernWinningNode((direction === 'row') ? row : i, (direction === 'row') ? i : col));
             }
         }
-        if (check === BOARD_SIZE) {
+        if (check === gameControls.BOARD_SIZE) {
             isWin = true;
         } else {
             clearArray(coords);
@@ -304,12 +290,12 @@ function checkWinner(gameBoard, boardDiv, row, col) {
             rowModifier = -1;
             colModifier = -1;
             expFindStart = (dr, dc) => dr > 0 && dc > 0;
-            expLoopControl = (i, j) => i < BOARD_SIZE && j < BOARD_SIZE;
+            expLoopControl = (i, j) => i < gameControls.BOARD_SIZE && j < gameControls.BOARD_SIZE;
         } else {
             rowModifier = 1;
             colModifier = -1;
-            expFindStart = (dr, dc) => dr < BOARD_SIZE - 1 && dc > 0;
-            expLoopControl = (i, j) => i >= 0 && j < BOARD_SIZE;
+            expFindStart = (dr, dc) => dr < gameControls.BOARD_SIZE - 1 && dc > 0;
+            expLoopControl = (i, j) => i >= 0 && j < gameControls.BOARD_SIZE;
         }
 
         let dr, dc;
@@ -321,12 +307,12 @@ function checkWinner(gameBoard, boardDiv, row, col) {
         }
         let diag1Check = 0;
         for (let i = dr, j = dc; expLoopControl(i, j); i -= rowModifier, j -= colModifier) {
-            if (gameBoard[i][j] !== null && gameBoard[i][j].getNumber() === currentPlayer.getNumber()) {
+            if (gameBoard[i][j] !== null && gameBoard[i][j].getNumber() === gameControls.currentPlayer.getNumber()) {
                 diag1Check++;
                 coords.push(discernWinningNode(i, j));
             }
         }
-        if (diag1Check === BOARD_SIZE) {
+        if (diag1Check === gameControls.BOARD_SIZE) {
             isWin = true;
         } else {
             clearArray(coords);
@@ -336,7 +322,7 @@ function checkWinner(gameBoard, boardDiv, row, col) {
 
     function processResult(winFound) {
         if (winFound) {
-            gameOver = true;
+            gameControls.gameOver = true;
             for (let i = 0; i < boardDiv.childNodes.length; i++) {
                 const node = boardDiv.childNodes[i];
                 node.classList.remove('empty');
@@ -356,7 +342,7 @@ function checkWinner(gameBoard, boardDiv, row, col) {
                 }
             }
             if (!spaceLeft) {
-                gameOver = true;
+                gameControls.gameOver = true;
                 for (let i = 0; i < boardDiv.childNodes.length && !spaceLeft; i++) {
                     const node = boardDiv.childNodes[i];
                     node.classList.remove('empty');
@@ -365,11 +351,11 @@ function checkWinner(gameBoard, boardDiv, row, col) {
                 }
             }
         }
-        if (gameOver) {
+        if (gameControls.gameOver) {
             const declareGameOver = document.createElement('div');
             declareGameOver.classList.add('game-declared-over');
             if (winFound) {
-                declareGameOver.textContent = currentPlayer.getName() + " wins!";
+                declareGameOver.textContent = gameControls.currentPlayer.getName() + " wins!";
             } else {
                 declareGameOver.textContent = "Draw!";
             }
